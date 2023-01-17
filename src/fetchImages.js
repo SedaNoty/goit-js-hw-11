06d8b62a775b9439146a3884d8816c6ca376510b
SedaNoty <sedanoty5@gmail.com>
2023-01-17 17:45:28 +0200

    edit 2

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,26 +1,4 @@
 
-// export const fetchImages = async (inputValue, pageNr) => {
-//   const url = 'https://pixabay.com/api/';
-//   const API_KEY = '32869255-acb467910c721877b1bf35e4f';
-//   return await fetch(
-//     `${url}?key=${API_KEY}&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
-//   )
-//     .then(async response => {
-//       if (!response.ok) {
-//         if (response.status === 404) {
-//           return [];
-//         }
-//         throw new Error(response.status);
-//       }
-//       return await response.json();
-//     })
-//     .catch(error => {
-//       console.error(error);
-//     });
-// };
-
-// ================================================
-
 import axios from 'axios';
 
 export async function fetchImages(inputValue, pageNr) {
