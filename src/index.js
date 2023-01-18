a2c9583614bb82a4585ac2517616b3d09ce3076d
SedaNoty <sedanoty5@gmail.com>
2023-01-18 02:43:58 +0200

    correct 4

--- src/index.js
+++ src/index.js
@@ -61,23 +61,25 @@ function renderImageList(images) {
       console.log('img', image);
       return `<div class="photo-card">
 
-       <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
+        <a href="${image.largeImageURL}">
+          <img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/>
+          </a>
 
-        <div class="info">
-           <p class="info-item">
-    <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
-</p>
+          <div class="info">
             <p class="info-item">
-                <b>Views</b> <span class="info-item-api">${image.views}</span>  
-            </p>
-            <p class="info-item">
-                <b>Comments</b> <span class="info-item-api">${image.comments}</span>  
-            </p>
-            <p class="info-item">
-                <b>Downloads</b> <span class="info-item-api">${image.downloads}</span> 
-            </p>
-        </div>
-    </div>`;
+                  <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
+              </p>
+              <p class="info-item">
+                  <b>Views</b> <span class="info-item-api">${image.views}</span>  
+              </p>
+              <p class="info-item">
+                  <b>Comments</b> <span class="info-item-api">${image.comments}</span>  
+              </p>
+              <p class="info-item">
+                  <b>Downloads</b> <span class="info-item-api">${image.downloads}</span> 
+              </p>
+          </div>
+      </div>`;
     })
     .join('');
   gallery.innerHTML += markup;
